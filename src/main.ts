290f4221974e0adcc5f812ee0bce04fd8e3c90cb
tsumo <tsumo@users.noreply.github.com>
2021-02-27 12:02:59 +0300

    feat: init canvas

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
-const a: number[] = [1, 2];
+const init = () => {
+  const canvas = document.getElementById("c");
+  if (canvas === null) {
+    console.error("Cannot find canvas element");
+    return;
+  }
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    console.error("Element is not a canvas");
+    return;
+  }
+  const ctx = canvas.getContext("2d");
+};
 
-console.log("Typescript works!", a);
+init();
